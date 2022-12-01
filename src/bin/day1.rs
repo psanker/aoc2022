18216073402dd7a0d02d23520ac75ef9fa1aa30b
Patrick Anker <patricksanker@gmail.com>
2022-12-01 16:04:27 -0500

    feat: add better implementation for day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,13 +1,13 @@
 use std::{fs::read_to_string, io::Error};
 
-pub fn day1pt1(path: &str) -> Result<i32, Error> {
+fn day1pt1(path: &str) -> Result<i32, Error> {
     let totals = get_totals(path)?;
 
     let max = totals.into_iter().fold(0, |acc, x| acc.max(x));
     Ok(max)
 }
 
-pub fn day1pt2(path: &str) -> Result<i32, Error> {
+fn day1pt2(path: &str) -> Result<i32, Error> {
     let mut totals = get_totals(path)?;
     totals.sort();
     let n = totals.len();
@@ -33,4 +33,27 @@ fn get_totals(path: &str) -> Result<Vec<i32>, Error> {
     });
 
     Ok(totals)
+}
+
+fn main() {
+    let d1p1 = day1pt1("inputs/day1.txt").ok().unwrap();
+    println!("Day 1 pt 1: {}", d1p1);
+
+    let d1p2 = day1pt2("inputs/day1.txt").ok().unwrap();
+    println!("Day 1 pt 2: {}", d1p2);
+
+    let (d1p1_better, d1p2_better) = day1_better("inputs/day1.txt").ok().unwrap();
+    println!("Day 1 better: ({}, {})", d1p1_better, d1p2_better);
+}
+
+fn day1_better(path: &str) -> Result<(i32, i32), Error>{
+    let mut totals = read_to_string(path)?
+        .split("\n\n")
+        .map(|chunk| chunk.lines().flat_map(|line| line.parse::<i32>()).sum())
+        .collect::<Vec<i32>>();
+
+    totals.sort();
+    totals.reverse();
+
+    Ok((totals[0], totals.iter().take(3).sum()))
 }
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-mod day1;
-
-fn main() {
-    let d1p1 = day1::day1pt1("inputs/day1.txt").ok().unwrap();
-    println!("Day 1 pt 1: {}", d1p1);
-
-    let d1p2 = day1::day1pt2("inputs/day1.txt").ok().unwrap();
-    println!("Day 1 pt 2: {}", d1p2);
-}
